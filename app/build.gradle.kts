07551f5d16af49c18cacbd81ecd92316a881b912
Juan Gil <juanmanuelgil99@gmail.com>
2024-01-14 00:36:12 -0300

    feat
    * Crear botón principal
    * Crear botón agregar
    * Crear Card de gasto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,12 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    // Room
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/jjapp/gastosdivididos/ui/composables/CommonComposables.kt
+++ app/src/main/java/com/jjapp/gastosdivididos/ui/composables/CommonComposables.kt
@@ -1,8 +1,11 @@
 package com.jjapp.gastosdivididos.ui.composables
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
@@ -12,14 +15,20 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jjapp.gastosdivididos.R
+import com.jjapp.gastosdivididos.data.models.Expense
 import com.jjapp.gastosdivididos.ui.theme.GastosDivididosTheme
 import com.jjapp.gastosdivididos.ui.theme.ColorPalette
+import com.jjapp.gastosdivididos.utils.DateTimeFormatters
+import java.time.LocalDateTime
 
 @Composable
 fun MainButton(
@@ -44,7 +53,10 @@ fun MainButton(
 
 @Composable
 fun AddButton(onClick: () -> Unit = {}) {
-    ElevatedButton(onClick = onClick, colors = buttonColors(containerColor = ColorPalette.Purple50)) {
+    ElevatedButton(
+        onClick = onClick,
+        colors = buttonColors(containerColor = ColorPalette.Purple50)
+    ) {
         val text = stringResource(id = R.string.add_btn_txt)
         Icon(
             imageVector = Icons.Default.Add,
@@ -60,8 +72,44 @@ fun AddButton(onClick: () -> Unit = {}) {
 fun MainButtonPreview() {
     GastosDivididosTheme {
         Column {
-            MainButton(onClick = {}, text = "Continue", color = ColorPalette.Purple30, enabled = true)
+            MainButton(
+                onClick = {},
+                text = "Continue",
+                color = ColorPalette.Purple30,
+                enabled = true
+            )
             AddButton(onClick = {})
         }
+    }
+}
+
+@Composable
+fun IconAndText(date: LocalDateTime, iconId: Int) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val image = painterResource(iconId)
+        Image(
+            painter = image,
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(ColorPalette.Neutral30),
+            modifier = Modifier
+                .padding(bottom = 4.dp)
+                .size(32.dp)
+        )
+        val dateText = date.format(DateTimeFormatters.ddmmyy)
+        Text(text = dateText, style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
+@Composable
+fun PrimaryAndSecondaryText(primaryText: String, secondaryText: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(text = primaryText, style = MaterialTheme.typography.bodyLarge)
+        Text(text = secondaryText, style = MaterialTheme.typography.bodyMedium)
     }
 }
